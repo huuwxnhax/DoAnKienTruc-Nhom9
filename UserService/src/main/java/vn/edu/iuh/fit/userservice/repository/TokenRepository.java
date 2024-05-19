65bcd0ab5657e0124c44fc7865b5a6af691956d0
huuwxnhax <111059487+huuwxnhax@users.noreply.github.com>
2024-05-19 19:06:51 +0700

    delete UserService

--- UserService/src/main/java/vn/edu/iuh/fit/userservice/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.iuh.fit.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.edu.iuh.fit.userservice.entity.Token;
-
-public interface TokenRepository
-        extends JpaRepository<Token, Long> {
-    Token findByToken(String token);
-}
